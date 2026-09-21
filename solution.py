d79a3a3ae384ce0cd97c82f2c834b9f8fdd880a8
will3702 <heatonwillem10@gmail.com>
2026-09-22 01:42:47 +1000

    Update solution.py

--- solution.py
+++ solution.py
@@ -283,7 +283,7 @@ class Solver:
         Check if Value Iteration has reached convergence.
         :return: True if converged, False otherwise
         """
-        {x: 0 for x in self.states}
+        
         #
         # TODO: Implement code to check if Value Iteration has reached convergence here.
         #
@@ -296,6 +296,23 @@ class Solver:
         """
         Perform a single iteration of Value Iteration (i.e. loop over the state space once).
         """
+        
+        self.values = {states: 0 for states in self.states}
+        self.gamma = self.environment.gamma
+        for states in self.states:
+            best = float("-inf")
+            for actions in self.valid_actions[s]:
+                self.transitions[(states, actions)] = outcomes
+                for p, s2 , r in outcomes:
+                    Vprev = self.values.get(s2, 0)
+                    V = p*(r + (self.gamm * Vprev))
+                    best = max(V, best) 
+            self.values[states] = best 
+                
+                
+            
+        
+            
         
         #
         # TODO: Implement code to perform a single iteration of Value Iteration here.
